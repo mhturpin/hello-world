05f5f74ee0575f7229f71a13744a5e2567b376f5
Maxwell Turpin <maxwell.turpin@tufts.edu>
2017-09-19 10:42:05 -0400

    Adding app files

---
+++ Google Drive/Documents/Code/AndroidStudioProjects/TinyApp/app/src/main/java/com/example/maxwell/tinyapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.maxwell.tinyapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button fullScreen = (Button) findViewById(R.id.full_screen_button);
+        fullScreen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), FullscreenActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+    }
+}
